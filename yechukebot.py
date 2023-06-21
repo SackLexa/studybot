e4b118ca1d2dcaa2550366a819a7919e5465d922
Алексей Еремийчук <snecers@list.ru>
2023-06-22 04:51:54 +0500

    added ephem functions

--- yechukebot.py
+++ yechukebot.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date
 import logging
 import settings
+import ephem
+
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,11 +18,41 @@ def greet_user(update, context):
     update.message.reply_text(text)
 
 def talk_to_me(update, context):
-    user_text = f'Привет, {update.message.chat.username}! Твоё сообщение: "{update.message.text}"'
+    user_text = f'Дорогой {update.message.chat.username}! Твоё сообщение: "{update.message.text}"'
     logging.info('User: %s, Message: %s, ChatID: %s', update.message.chat.username, 
                  update.message.text, update.message.chat.id)
     update.message.reply_text(user_text)
 
+def planet_today(update, context):
+    planet = update.message.text.split()[-1].lower()
+    today = date.today().strftime("%Y/%m/%d")
+    logging.info('User: %s, Message: %s, ChatID: %s', update.message.chat.username, 
+                 update.message.text, update.message.chat.id)
+    if planet == 'mercury':
+        const = ephem.Mercury(today)
+    elif planet == 'venus':
+        const = ephem.Venus(today)
+    elif planet == 'earth':
+        update.message.reply_text('Ты же сам_а на Земле!')
+    elif planet == 'mars':
+        const = ephem.Mars(today)
+    elif planet == 'jupiter':
+        const = ephem.Jupiter(today)
+    elif planet == 'saturn':
+        const = ephem.Saturn(today)
+    elif planet == 'uranus':
+        const = ephem.Uranus(today)
+    elif planet == 'neptune':
+        const = ephem.Neptune(today)
+    elif planet == 'pluto':
+        const = ephem.Pluto(today)
+    elif planet == 'moon':
+        const = ephem.Moon(today)         
+    else:
+        update.message.reply_text(f'Я не знаю "{planet.capitalize()}". \nВведи ещё раз название планеты в формате: \n"/planet название_планеты_на_английском"')
+    position_of_planet = ephem.constellation(const)
+    update.message.reply_text(f'Сейчас {planet.capitalize()} в созвездии {position_of_planet[1]}')
+               
 
 def main():
     mybot = Updater(settings.BOT_KEY, use_context=True)
@@ -28,6 +61,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_today))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
